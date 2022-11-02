70cbd0b422413adeb21d504df28f681089ce8187
emremataraci <mtrcemre@gmail.com>
2022-11-02 04:33:17 +0300

    books and book clubs pages are wrapped with map function

--- src/Modules/Books.js
+++ src/Modules/Books.js
@@ -3,33 +3,29 @@ import { Image, Stack } from '@chakra-ui/react'
 import './Books.css';
 import React, { useState } from 'react';
 import Modal from '../Components/Shared/Pop-up/Modal';
-
-
+import Data from './Data';
 
 function Books() {
   const [openModal, setOpenModal] = useState(false);
 
   return (
+    
     <div>
-        <ul>
-            <Stack direction='row' spacing={4} className= "homepage-books">
-            <li>
-            <Link to="/Book1"><Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/150' />
-            </Link>
-            <button onClick={() => setOpenModal(true)} className='modalButton'>Kitabı İncele</button><Modal open={openModal} onClose={() => setOpenModal(false)} />
+      <ul>
 
-            </li> 
-            <li>
-            <Link to="/Book2"><Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/150' /></Link>
-            </li> 
-            <li>
-            <Link to="/Book3"><Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/150' /></Link>
-            </li> 
-            <li>
-            <Link to="/Book4"><Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/150' /></Link>
-            </li> 
-            </Stack>
-        </ul>
+      {Data.map((item,index) => {
+        return(
+          <Stack direction='row' spacing={4} className= "homepage-books">
+                <li className='block'>         
+          <Link to={item.link}><Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/150' />
+            </Link>       
+            <button onClick={() => setOpenModal(true)} className='modalButton'>Kitabı İncele</button><Modal open={openModal} onClose={() => setOpenModal(false)} />
+            </li>
+          </Stack>
+        )
+      })}
+
+      </ul>      
     </div>
   )
 }
--- src/Modules/Data.js
+++ src/Modules/Data.js
@@ -5,37 +5,46 @@ const Data = [
     {
       id: 1,
       title: 'Martin Eden?',
-      img: <Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/350' className='book-img'/>,
+      img: 'gibbresh',
       info:
         'lk kez Ethereum’un kurucularından Gavin Wood’un 2014’te kullandığı “Web 3” terimi, “token” denilen blok birimleri esas aldığı için günümüzdeki merkeziyetçilikten uzak olan yepyeni bir internet anlamına geliyor. ',
-    },
+      link: 'Book1'
+      },
     {
       id: 2,
       title: 'Sıfırdan Bire',
-      img: <Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/350' className='book-img'/>,
+      img: 'gibbresh',
       info:
         'Validator, blokzincir ağında kullanılan proof of stake mekanizmasının önemli bir parçasıdır. Proof of work sisteminde madencinin aldığı rolü üstlenirler ve cihazlar yerine blok ekleme ve doğrulama işlemlerini yaparlar. Blokzincir üzerindeki işlemler, yalnızca validator tarafından onaylandığında tamamlanmış olur. Yani, zincirin kilit halkasıdır.',
-    },
+      link: 'Book2'
+
+      },
     {
       id: 3,
       title: 'Simyacı',
-      img: <Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/350' className='book-img'/>,
+      img: 'gibbresh',
       info:
         'Sıfır bilgi ispatı, bilgisayar bilimlerinde veri güvenliği alanında kullanılan algoritmanın temel prensibi "bildiğiniz bilgiyi bir başkasına, bilgiyi ona vermeden ispat etmektir. Bilginizi ispat etme yönteminiz genel olarak ideal bir kriptografik fonksiyonunun varsayımlarıdır.',
-    },
+      link: 'Book3'
+
+      },
     {
       id: 4,
       title: "Kürk Mantolu Madonna",
-      img: <Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/350' className='book-img'/>,
+      img: 'bookjpeg',
       info:
         "İki üst ve renk körü arkadaş Arkadaşınızın kırmızı-yeşil renk körü (siz değilken) ve iki top hayaliniz edin: bir kırmızı ve biri yeşil, bunun yanında aynı . Arkadaşınıza tamamen aynı görünüyor ve aslında aslında olduklarından şüpheleniyor. Gercekte farklı renkte bilgisine sahip olmak istiyorsun, ama başka bir şey değil; özellikle hangisinin kırmızı, hangisinin yeşil top oldugunu açıklamakezsiniz.İşte ispat sistemi. İki topu arkadaşınıza veriyorsunuz ve o onları arkasına koyuyor. Sonra toplardan birini alıp arkasından çıkarır ve gösterir. Daha sonra tekrar arkasına yerleştirir ve ardından iki toptan birini ortaya çıkarmaktan seçer ve ikisinden bir olasılıkla seçer. Size 'Topu değiştirdim mi?' Diye soracak. Tüm bu prosedür daha sonra kadar tekrar edilir. Renklerine, tabii ki, değiştirilmediği kesin olarak söyleyebilirsin. Öte yandan, aynı renkteyse ve garanti edilemezlerse,% 50'den yüksek olasılıkla doğru tahmin etmenin hiçbir yolu yoktur. Her bir anahtarı / anahtarı olmayanları tanımlamada başarılı olma olasılığınız% 50, tüm anahtarlı / anahtar olmayanlarda başarılı başarılı olma sıfıra yaklaşır ('sağlamlık'). Siz ve arkadaşınız bu 'kanıtı' defalarca (söz 100 kez) tekrar ederseniz, arkadaşınız topların gerçekten farklı renkte ikna olmalıdır ('cumalık'). üçüncü kanıt sıfır bilgidir çünkü arkadaşınız hangi topun yeşil, hangisinin kırmızı asla öğrenmez; gerçekten de yetenek nasıl edeceği konusunda hiçbir bilgisi yok.",
-    },
+      link: 'Book4'
+
+      },
     {
       id: 5,
       title: 'Oliver Twist',
-      img: <Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/350' className='book-img'/>,
+      img: 'bookjpeg',
       info:
         'Bu proje Emre Mataracı tarafından egzersiz amaçlı yapılmıştır.',
-    },
+      link: 'Book1'
+
+      },
   ]
   export default Data
