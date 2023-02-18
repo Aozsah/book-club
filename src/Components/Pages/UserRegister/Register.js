2d2f7944817e39ff1f433db5485da87905b40a97
emremataraci <mtrcemre@gmail.com>
2023-02-18 15:44:14 +0300

    Unnecessary files removed

--- src/Components/Pages/UserRegister/Register.js
+++ src/Components/Pages/UserRegister/Register.js
@@ -117,10 +117,7 @@ export default function Register() {
                 
               </Grid>
               <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="Buraya promosyon mail mesajı yazılacak!"
-                />
+                
               </Grid>
             </Grid>
             <Button
